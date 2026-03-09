0723c18b255c7e465c9dbf055f4c04adb7523ca5
frossq <francesco.asquini@secomind.com>
2026-03-09 16:44:22 +0100

    feat(pairing): add functions to create/delete keypairs in OpenBao
    - Added OpenBao setup steps in docker compose + CI
    - Added tests
    
    Signed-off-by: frossq <francesco.asquini@secomind.com>

--- apps/astarte_pairing/lib/astarte_pairing/fdo/open_bao/core.ex
+++ apps/astarte_pairing/lib/astarte_pairing/fdo/open_bao/core.ex
@@ -20,4 +20,57 @@ defmodule Astarte.Pairing.FDO.OpenBao.Core do
   @moduledoc """
   Implementation of function to interface with OpenBao.
   """
+
+  alias Astarte.Pairing.FDO.OpenBao.Client
+
+  require Logger
+
+  @type key_algorithm :: :ec256 | :ec384 | :rsa2048 | :rsa3072
+
+  @spec key_type_to_string(key_algorithm()) :: {:ok, String.t()} | :error
+  def key_type_to_string(key_type) do
+    case key_type do
+      :ec256 -> {:ok, "ecdsa-p256"}
+      :ec384 -> {:ok, "ecdsa-p384"}
+      :rsa2048 -> {:ok, "rsa-2048"}
+      :rsa3072 -> {:ok, "rsa-3072"}
+      _ -> :error
+    end
+  end
+
+  @spec create_keypair(String.t(), String.t(), boolean(), String.t() | nil) ::
+          :error | {:error, Jason.DecodeError.t()} | {:ok, any()}
+  def create_keypair(key_name, key_type, allow_key_export_and_backup, auth_token) do
+    req_body =
+      %{
+        type: key_type,
+        exportable: allow_key_export_and_backup,
+        allow_plaintext_backup: allow_key_export_and_backup
+      }
+      |> Jason.encode!()
+
+    headers = build_custom_headers(auth_token)
+
+    case Client.post("/v1/transit/keys/#{key_name}", req_body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: resp_body}} ->
+        with {:ok, decoded_resp_body} <- Jason.decode(resp_body) do
+          Map.fetch(decoded_resp_body, "data")
+        end
+
+      error_resp ->
+        Logger.error("Encountered HTTP error #{inspect(error_resp)}")
+        :error
+    end
+  end
+
+  # custom headers to properly interact with OpenBao
+  @spec build_custom_headers(String.t() | nil) :: list()
+  def build_custom_headers(custom_token) do
+    base_header = [{"Content-Type", "application/json"}]
+
+    case custom_token do
+      nil -> base_header
+      token -> [{"X-Vault-Token", token} | base_header]
+    end
+  end
 end
--- apps/astarte_pairing/lib/astarte_pairing/fdo/open_bao/open_bao.ex
+++ apps/astarte_pairing/lib/astarte_pairing/fdo/open_bao/open_bao.ex
@@ -18,6 +18,55 @@
 
 defmodule Astarte.Pairing.FDO.OpenBao do
   @moduledoc """
-  Functionality to interface with OpenBao.
+  Functionality to interface with OpenBao APIs.
   """
+
+  alias Astarte.Pairing.FDO.OpenBao.{Client, Core}
+
+  require Logger
+
+  @spec create_keypair(String.t(), Core.key_algorithm(), list()) ::
+          {:ok, map()} | {:error, Jason.DecodeError.t()} | :error
+  def create_keypair(key_name, key_type, opts \\ []) do
+    allow_key_export_and_backup = Keyword.get(opts, :allow_key_export_and_backup, false)
+    auth_token = Keyword.get(opts, :auth_token, nil)
+
+    with {:ok, key_type_string} <- Core.key_type_to_string(key_type) do
+      Core.create_keypair(key_name, key_type_string, allow_key_export_and_backup, auth_token)
+    end
+  end
+
+  @spec enable_key_deletion(String.t()) :: {:ok, map()} | :error
+  def enable_key_deletion(key_name, opts \\ []) do
+    auth_token = Keyword.get(opts, :auth_token, nil)
+
+    req_body = %{deletion_allowed: true} |> Jason.encode!()
+
+    headers = Core.build_custom_headers(auth_token)
+
+    case Client.post("/v1/transit/keys/#{key_name}/config", req_body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        {:ok, %{}}
+
+      error_resp ->
+        Logger.error("Encountered HTTP error #{inspect(error_resp)}")
+        :error
+    end
+  end
+
+  @spec delete_key(String.t()) :: {:ok, map()} | :error
+  def delete_key(key_name, opts \\ []) do
+    auth_token = Keyword.get(opts, :auth_token, nil)
+
+    headers = Core.build_custom_headers(auth_token)
+
+    case Client.delete("/v1/transit/keys/#{key_name}", headers) do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
+        {:ok, %{}}
+
+      error_resp ->
+        Logger.error("Encountered HTTP error #{inspect(error_resp)}")
+        :error
+    end
+  end
 end
